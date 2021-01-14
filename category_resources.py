ef60f4579930aa3a7278dcf7cec64658ccf494ec
Mykhailo4444 <73576413+Mykhailo4444@users.noreply.github.com>
2021-01-14 16:42:41 +0200

    Bot

---
+++ category_resources.py
@@ -0,0 +1,52 @@
+from flask_restful import Resource, Api
+from flask import request
+from shop_models import Category
+from marshmallow.exceptions import ValidationError
+from marshmallow import Schema
+from marshmallow import fields
+from marshmallow.validate import Length
+from flask import Flask
+
+
+class CategorySchemaRead(Schema):
+    title = fields.String(required=True)
+    description = fields.String(validate=Length(max=512))
+
+
+class CategoryResource(Resource):
+
+    def get(self, id1=None):
+        if id1:
+            return CategorySchemaRead().dump(Category.objects.get(id=id1))
+        else:
+            category = Category.objects()
+            return CategorySchemaRead().dump(category, many=True)
+
+    def post(self):
+        try:
+            CategorySchemaRead().load(request.json)
+        except ValidationError as e:
+            return {'text': str(e)}
+        category = Category(**request.json).save()
+        return CategorySchemaRead().dump(category)
+
+    def put(self, id1=None):
+        try:
+            CategorySchemaRead().load(request.json)
+        except ValidationError as e:
+            return {'text': str(e)}
+
+        category = Category.objects.get(id=id1)
+        category.update(**request.json)
+        return CategorySchemaRead().dump(category)
+
+    def delete(self, id1=None):
+        category = Category.objects.get(id=id1)
+        category.delete()
+        return {'text': 'was deleted'}
+
+
+app = Flask(__name__)
+api = Api(app)
+api.add_resource(CategoryResource, '/', '//<string:id1>')
+app.run(debug=True)
---
+++ send_notifications.py
@@ -0,0 +1,16 @@
+from sending_news import Sender
+from shop_models import User
+from extra_models import News_hot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
+count_news = News_hot.objects().count()
+last_news = News_hot.objects[count_news-1:count_news]
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+a = ['Свіжі Новини', '/start']
+buttons = [KeyboardButton(n) for n in a]
+kb.add(*buttons)
+for i in last_news:
+    s = Sender(User.objects(), text=i.body, reply_markup=kb)
+    s.send_message()
+
+
